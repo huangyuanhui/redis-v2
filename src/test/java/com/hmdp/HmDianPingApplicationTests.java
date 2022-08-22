a6dc173885473f0ee3669ea6eff0fc77812c7910
huangyuanhui <2278181701@qq.com>
2022-08-22 23:41:27 +0800

    十三：缓存工具类封装

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,10 +1,16 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.StringRedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -12,10 +18,17 @@ class HmDianPingApplicationTests {
     @Autowired
     private ShopServiceImpl shopService;
 
+    @Autowired
+    private CacheClient cacheClient;
+
     @Test
     public void testSaveShop() {
         // 缓存预热
-        shopService.saveShop2Redis(1L, 20L);
+        // shopService.saveShop2Redis(1L, 20L);
+
+        // 缓存工具类实现缓存重建
+        Shop shop = shopService.getById(1L);
+        cacheClient.setWithLogicExpire(CACHE_SHOP_KEY + 1L, shop, CACHE_SHOP_TTL, TimeUnit.MINUTES);
     }
 
 }
